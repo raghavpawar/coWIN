175679ccb464b2f46e9bdb22d75e302a2e42f639
ishan <ishan_d@es.iitr.ac.in>
2021-05-18 16:53:22 +0530

    added drop down menu

---
+++ lib/Apicalls/ApiByDistrict.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class District {
+  District({
+    this.districtId,
+    this.districtName,
+  });
+
+  int districtId;
+  String districtName;
+
+  factory District.fromJson(Map<String, dynamic> json) => District(
+        districtId: json["district_id"],
+        districtName: json["district_name"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "district_id": districtId,
+        "district_name": districtName,
+      };
+}
